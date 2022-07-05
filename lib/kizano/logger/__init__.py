a81eb2f106779ffba0f9c150589ec17b977de9f3
Markizano Draconus <markizano@markizano.net>
2022-07-04 21:26:31 -0500

    Adding env support for setting/getting the logger level.

--- lib/kizano/logger/__init__.py
+++ lib/kizano/logger/__init__.py
@@ -1,4 +1,5 @@
 
+import os
 import sys
 import logging
 from logging.handlers import SysLogHandler
@@ -7,6 +8,7 @@ class LocalSyslogHandler(SysLogHandler):
     def emit(self, record):
         '''
         Takes a log message and breaks it out into multiple messages if it exceeds 64k for syslogger.
+        Overload function since the original will throw an error if message is over the limit.
         '''
         maxlen = 2015 if sys.platform == 'darwin' else 65502
         mesg = record.getMessage()
@@ -18,7 +20,21 @@ class LocalSyslogHandler(SysLogHandler):
         else:
             SysLogHandler.emit(self, record)
 
-def getLogger(name, log_level=logging.DEBUG):
+def getLogger(name, log_level=None):
+    '''
+    Get a logger by the name provided. Set the log_level if you provide the second argument.
+    If not, ask the environment for $LOG_LEVEL. If not, default to DEBUG.
+    Also attaches a syslog handler that will handle large strings and stream the message
+    to the syslog endpoint.
+    @param log_level {enum:string} One of the logger levels.
+    @return {@logging.getLogger()}
+    '''
+    log_levels = ['DEBUG', 'INFO', 'WARN', 'WARNING', 'ERROR', 'FATAL', 'CRITICAL']
+    log_level_map = dict([ (_ll, getattr(logging, _ll)) for _ll in log_levels ])
+    if not log_level:
+        #@throws KeyError if $LOG_LEVEL is set to invalid log level as described
+        # by above array $log_level_map
+        log_level = log_level_map[ os.getenv('LOG_LEVEL', 'DEBUG').upper() ]
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     logging.basicConfig(format='%(asctime)s %(name)s.%(funcName)s(PID=%(process)d, %(levelname)s) %(message)s')
