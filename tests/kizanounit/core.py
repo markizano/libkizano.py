a937b57cfe33872311f74db130cba8323f44fb8a
Markizano Draconus <markizano@markizano.net>
2023-04-07 14:57:39 -0500

    include testing and updates to logging.

--- tests/kizanounit/core.py
+++ tests/kizanounit/core.py
@@ -10,7 +10,7 @@ class TestCore(unittest.TestCase):
     import kizano
     log = kizano.getLogger(__name__, 10)
     log.error('ERROR')
-    log.warn('WARNING')
+    log.warning('WARNING')
     log.info('INFO')
     log.debug('DEBUG')
     log.info('__REALLY_LONG_MESSAGE__' * 2849)
